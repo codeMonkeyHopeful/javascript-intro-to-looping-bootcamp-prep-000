6141e4ed44bbaa1b6a88889c9ab44bde125476fc
Ryan Jasinski <jasinski.17@gmail.com>
2019-01-22 16:42:27 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -17,11 +17,8 @@ function whileLoop(n) {
 function incrementVariable() {
   i = i + 1;
 }
-
-function doWhileLoop(array) {
-  var i = 0;
-  do {
-    array.pop();
-    incrementVariable();
-  } while(array.length > 0 && array[incrementVariable()] !== -1)
-}
+ 
+do {
+  console.log("doo-bee-doo-bee-doo");
+  incrementVariable();
+} while (i < 5);
